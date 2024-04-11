2fc08624684101823caf6746a264db0f4e3af8ba
Mandiseli <mandiseli@outlook.com>
2024-04-11 15:07:33 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+  <div>
+    <header className='p-4'>
+      <a href="" className="flex items-center gap-1">MM<span className='font-bold text-xl'>TRAVELS</span></a>
+    </header>
+  </div>
+  )
+}
+
+export default App
