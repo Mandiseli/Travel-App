1d70646c016e9144e4434b0b05d653957c56589c
Mandiseli <mandiseli@outlook.com>
2024-04-16 11:26:03 +0200

    working on index page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,20 @@
 import './App.css'
+import {Route, Routes} from "react-router-dom";
+import IndexPage from './pages/Indexpage';
+import LoginPage from './pages/LoginPage';
+import Layout from './Layout';
 
 function App() {
   return (
-  <div>
-    <header className='p-4 flex justify-between'>
-      <a href="" className="flex items-center gap-1">
-        MM<span className='font-bold text-xl'>TRAVELS</span>
-        </a>
-      <div className='flex gap-2 border border-blue-300 rounded-full py-2 px-4 shadow-md shadow-blue-500 '>
-        <div>Anywhere</div>
-        <div className='border-l border blue-300'></div>
-        <div>Any week</div>
-        <div className='border-l border blue-300'></div>
-        <div>Add Guests</div>
-        <button className='bg-primary tect-white p-2 rounded-full'>
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 129 129" 
-            xmlns:xlink="http://www.w3.org/1999/xlink" 
-            enable-background="new 0 0 129 129">
-          <g>
-            <path d="M51.6,96.7c11,0,21-3.9,28.8-10.5l35,35c0.8,0.8,1.8,1.2,2.9,1.2s2.1-0.4,2.9-1.2c1.6-1.6,1.6-4.2,0-5.8l-35-35   
-                  c6.5-7.8,10.5-17.9,10.5-28.8c0-24.9-20.2-45.1-45.1-45.1C26.8,6.5,6.5,26.8,6.5,51.6C6.5,76.5,26.8,96.7,51.6,96.7z M51.6,14.7   
-                  c20.4,0,36.9,16.6,36.9,36.9C88.5,72,72,88.5,51.6,88.5c-20.4,0-36.9-16.6-36.9-36.9C14.7,31.3,31.3,14.7,51.6,14.7z"></path>
-          </g>
-        </svg>
-        </button>
-      </div>
-    </header>
-  </div>
+
+    <Routes>
+      <Route path="/" element={<Layout />}> 
+      <Route index element={<IndexPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      </Route>
+     
+    </Routes>
+  
   )
 }
 
---
+++ client/src/Header.jsx
@@ -0,0 +1,37 @@
+export default function Header(){
+    return(
+        <header className='p-4 flex justify-between'>
+        <a href="" className="flex items-center gap-1">
+          MM<span className='font-bold text-xl'>TRAVELS</span>
+          </a>
+        <div className='flex gap-2 border border-blue-300 rounded-full py-2 px-4 shadow-md shadow-blue-500 '>
+          <div>Anywhere</div>
+          <div className='border-l border blue-300'></div>
+          <div>Any week</div>
+          <div className='border-l border blue-300'></div>
+          <div>Add Guests</div>
+          <button className='bg-primary text-white p-2 rounded-full'>
+          <i class="bx bx-search about_icon"></i>
+         
+          </button>
+        </div>
+        <Link to={"/login"} className='flex items-centre gap-2 border border-blue-300 rounded-full py-2 px-4'>
+  
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 relative top-1">
+          <path fill-rule="evenodd" 
+                d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" 
+                clip-rule="evenodd" />
+        </svg>
+        <div className=''>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6 relative top-1">
+          <path fill-rule="evenodd" 
+                d="M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" 
+                clip-rule="evenodd" />
+        </svg>
+  
+        </div>
+  
+        </Link>
+      </header> 
+    )
+}  
