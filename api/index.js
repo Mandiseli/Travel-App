ad744a8298f167fcc7ecb5a0ca13ad13e98fa16d
Mandiseli <mandiseli@outlook.com>
2024-04-19 09:46:24 +0200

    working on authentication

--- api/index.js
+++ api/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require("mongoose")
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 const User = require('./models/User.js')
 require('dotenv').config()
 const app = express();
 
-const bcryptSalt = bcrypt.genSalt(12)
+const bcryptSalt = bcrypt.genSaltSync(12)
 
 app.use(express.json())
 app.use(cors({
@@ -20,14 +21,39 @@ app.get('/test', (req,res) =>{
     res.json('test ok');
 });
 
-app.post('/register', (req,res) =>{
+app.post('/register', async (req,res) =>{
     const {name,email,password} = req.body;
-    User.create({
-        name,
-        email,
-        password:bcrypt.hashSync(password, secret),
-    })
-    res.json({name,email,password});
+
+    try{
+        const userDoc = await User.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcryptSalt),
+        });
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e);
+    }
+   
+    
 })
+
+app.post('/login', async(req,res) =>{
+    const {email, password} = req.body;
+    const userDoc = await User.findOne({email})
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk){
+            jwt.sign({email:userDoc.email, id:userDoc._id})
+            res.cookie('token', '').json('password Ok')
+        } else {
+            res.status(422).json('password not ok')
+        }
+    } else {
+        res.json('Not found')
+    }
+})
+
+
 
 app.listen(4000);
